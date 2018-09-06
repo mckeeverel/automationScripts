83186bedba9ccf820a81e377f4b44785c059e668
Eli McKeever <36924622+mckeeverel@users.noreply.github.com>
2018-09-05 21:39:57 -0500

    Update parseEmailForSECFilings_S1.ps1

--- parseEmailForSECFilings_S1.ps1
+++ parseEmailForSECFilings_S1.ps1
@@ -1,13 +1,16 @@
 ﻿
+# this section is contructing the outlook object 
 Add-Type -Assembly "Microsoft.Office.Interop.Outlook"
 $Outlook = New-Object -ComObject Outlook.Application
 $namespace = $Outlook.GetNameSpace("MAPI")
 
 
 #### S-1 ####
-
+# here we navigate the outlook object to find items that are unread and assign them to the $unread variable. The values in parenthesis will vary.
 $unread = $namespace.Folders.Item(1).Folders.Item(23).Folders.Item(1).items | where {$_.UnRead -eq "True"}
+# make a new array, $urls
 $urls = @()
+#loop through our $unread messages and do some clunky parsing for the link text. Add that link text to the $urls array. Mark the message as read.
 foreach($message in $unread){
    $a,$b = $message.Body -split "<"
    $url,$d = $b -split ">"
@@ -15,6 +18,7 @@ foreach($message in $unread){
    $message.UnRead = "False"
 }
 
+#loop through the $urls array and create a new browser window for each
 foreach($url in $urls){
 $ie = New-Object -ComObject InternetExplorer.Application
 $ie.Visible = $true
@@ -22,8 +26,9 @@ $ie.navigate2($url)
     while($ie.busy){
         Start-Sleep -Seconds 1
     }
-
+# in each window, we traverse the DOM and find the appropriate link to click and stash it in a variable $link
 $link = $ie.Document.getElementsByTagName("a") | where {$_.href -like "*/Archives/edgar/data*"} | select -Index 0
+# here we click that link to open the document
 $link.click()
 }
 
